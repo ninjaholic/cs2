be8af7c1b0aa5fcaf324a53171fb766dd143ff4c
ninjaholic <68186786+ninjaholic@users.noreply.github.com>
2025-04-07 16:20:08 +0000

    Update scoreboard_crop_dropfile_cs2.bat

--- scoreboard_crop_dropfile_cs2.bat
+++ scoreboard_crop_dropfile_cs2.bat
@@ -1,4 +1,4 @@
-:: Drop CS2 scoreboard screenshot jpegs onto this .bat file one at a time
+:: Drop CS2 scoreboard screenshot images onto this .bat file one at a time
 :: to crop them with ImageMagick
 :: Requires ImageMagick to function -- https://imagemagick.org
 ::
